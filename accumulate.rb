d3dea329c6e340f050224563a048c15e81cd044e
Leijun JIANG <leijun.jiang@soprahr.com>
2018-09-18 17:45:37 +0200

    done !

--- accumulate.rb
+++ accumulate.rb
@@ -9,9 +9,21 @@ class Array
 
 		elsif block_given?
 
+			i = 0
+			while i < self.length do
+				out << block.call(self[i])
+				i += 1
+			end
+
+			out
+
+
+
+
 			#out_array = Array.new self.size
-			self.each { |v| out_array << block.call(v) }
-			out_array
+			#self.each { |v| out << block.call(v) }
+			#out
+
 		end
 	end
 end
